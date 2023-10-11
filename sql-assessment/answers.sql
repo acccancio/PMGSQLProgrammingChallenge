525acf31009a88cb695ca32a005e8506ea13a92e
acccancio <112276045+acccancio@users.noreply.github.com>
2023-10-11 00:08:32 -0400

    Add files via upload
    PMG SQL Assessment by Adrian Christopher Cancio

--- sql-assessment/answers.sql
+++ sql-assessment/answers.sql
@@ -32,17 +32,19 @@ ORDER BY SUM(conversions) DESC;
 
 #5
 SELECT campaign_info.name AS campaign_name, 
-         SUM(marketing_performance.cost) AS total_cost, 
-         SUM(marketing_performance.impressions) AS total_impressions, 
-         SUM(marketing_performance.clicks) AS total_clicks, 
-         SUM(website_revenue.revenue) AS total_revenue, 
-         SUM(marketing_performance.cost)/SUM(marketing_performance.clicks) AS cost_per_click, 
-         SUM(marketing_performance.clicks)/SUM(marketing_performance.impressions) AS clickthough_rate, 
-         SUM(marketing_performance.conversions)/SUM(marketing_performance.clicks)*100 AS conversion_rate, 
-         SUM(website_revenue.revenue)/SUM(marketing_performance.cost)*100 AS ROAS
+      SUM(marketing_performance.cost) AS total_cost, 
+      SUM(marketing_performance.impressions) AS total_impressions, 
+      SUM(marketing_performance.clicks) AS total_clicks, 
+      SUM(marketing_performance.clicks)/SUM(marketing_performance.impressions) AS clickthough_rate, 
+      SUM(marketing_performance.cost)/SUM(marketing_performance.clicks) AS cost_per_click, 
+      SUM(marketing_performance.conversions) AS total_conversions, 
+      SUM(marketing_performance.conversions)/SUM(marketing_performance.clicks)*100 AS conversion_rate, 
+      SUM(marketing_performance.cost)/SUM(marketing_performance.conversions) AS cost_per_acquisition,   
+      SUM(website_revenue.revenue) AS total_revenue, 
+      SUM(website_revenue.revenue)/SUM(marketing_performance.cost)*100 AS ROAS
 FROM ((marketing_performance 
-         INNER JOIN campaign_info ON campaign_info.id = marketing_performance.campaign_id) 
-         INNER JOIN website_revenue ON marketing_performance.campaign_id = website_revenue.campaign_id)
+      INNER JOIN campaign_info ON campaign_info.id = marketing_performance.campaign_id) 
+      INNER JOIN website_revenue ON marketing_performance.campaign_id = website_revenue.campaign_id)
 GROUP BY campaign_info.name;
 
 'Answer: In terms of efficiency in generating revenue while maintaining low cost, Campaign 2 would be most efficient. Despite a relatively high CPC, the campaign succeeds in generating a high conversation rate of 30.67% and a low CPA of $0.44. It could be given that Campaign 3 may be arguably better in performance, but this could be mainly due to a high investment during its whole campaign duration, with a much lower ROAS.'
